143422be0ca4e5c56a4101c177324cb024ae366e
Bizzonium <azizonkg@gmail.com>
2022-06-21 02:07:39 +0300

    Initial commit

---
+++ config/slightguimodifications/cts.groovy
@@ -0,0 +1,113 @@
+// Here is the groovy file for screen elements
+info("Hello From 'Slight' Gui Modifications cts script")
+
+mainMenu {
+    enabled = true
+
+    splashText {
+        enabled = true
+        splashesEnabled = false
+    }
+
+    background {
+        clearBackgrounds()
+        backgroundStayLength = 50000 // 50 seconds
+        backgroundFadeLength = 2000 // 2 seconds fade
+        renderGradientShade = false
+        image { texture = file("config/slightguimodifications/background.png") }
+    }
+
+    removeMinecraftLogo()
+    removeEditionBadge()
+    clearAllButtons()
+
+    label {
+        position {
+            x = 2
+            y {
+                it - 20
+            }
+        }
+
+        text = literal("SS220 - Кастомный All of Fabric 5 - Версия 0.3.0")
+    }
+
+    button {
+        position {
+            x {it / 10 + 105}
+            y { it / 2 + 15}
+        }
+        width = 100
+        height = 20
+
+        text = literal("Моды")
+        onClicked = modMenu()
+    }
+    button {
+        position {
+            x {it / 10}
+            y { it / 2 + 15}
+        }
+        width = 100
+        height = 20
+
+        text = literal("Языки")
+        onClicked = language()
+    }
+    button {
+        position {
+            x {it / 10}
+            y {it / 2 - 60}
+        }
+        width = 204
+        height = 20
+		align = "left"
+        text = translatable("menu.singleplayer")
+        onClicked = singleplayer()
+    }
+    button {
+        position {
+            x {it / 10}
+            y {it / 2 - 35}
+        }
+        width = 204
+        height = 20
+
+        text = translatable("menu.multiplayer")
+        onClicked = multiplayer()
+    }
+    
+    button {
+        position {
+            x {it / 10 + 105}
+            y { it / 2 + 40}
+        }
+        width = 100
+        height = 20
+
+        text = translatable("menu.quit")
+        onClicked = exit()
+    }
+    button {
+        position {
+            x {it / 10}
+            y { it / 2 + 40}
+        }
+        width = 100
+        height = 20
+
+        text = translatable("menu.options")
+        onClicked = options()
+    }
+    button {
+        position {
+            x {it / 10}
+            y { it / 2 - 10}
+        }
+        width = 204
+        height = 20
+
+        text = literal("SS220 Games Discord")
+        onClicked = url("https://games.ss220.space")
+    }
+}
